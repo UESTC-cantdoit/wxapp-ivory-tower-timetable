3f169d8a29e0d570f928fd2c419a0bce3cf7d20d
宋璟珅 <lolipop0703@qq.com>
2020-06-05 14:53:08 +0800

    little changes

--- pages/course/course.js
+++ pages/course/course.js
@@ -4,6 +4,10 @@ Page({
    * 页面的初始数据
    */
   data: {
+    displayDayNum: 7, // 显示的天数，可选值为“5”和“7”
+    displayDay: ['一', '二', '三', '四', '五', '六', '七'],
+    displayCourseNum: 14, // 显示的每天课程数，可选值为“11”至“14”
+    displayCourse: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
     colorArrays: ["#FC9F9D", "#0A9A84"],
     wlist: [
       { "xqj": 1, "sksj": 1, "skcd":2, "kcxx": "微积分 品学楼101 1~16周"},
@@ -19,6 +23,7 @@ Page({
       { "xqj": 5, "sksj": 3, "skcd": 2, "kcxx": "软件工程 品学楼403 1~16周" },
     ]
   },
+
   showCardView: function (e) {
     wx.navigateTo({
       url: 'courseModify?courseId=' + e.currentTarget.courseId
@@ -33,6 +38,26 @@ Page({
     var skcd = wx.getStorageSync('skcd');
     this.setData({ kcxx: kcxx });
     this.setData({ skcd: skcd});
+
+    // 获取显示的天数，更新 displayDay
+    if (this.data.displayDayNum != 7) {
+      this.setData({
+        displayDay: ['一', '二', '三', '四', '五']
+      });
+    }
+
+    // 获取显示的课程节数，更新 displayCourse
+    if (this.data.displayCourseNum != 14) {
+      const num = this.data.displayCourseNum;
+      let array = [];
+      for (let count = 1; count <= num; count++) {
+        array.push(count);
+      }
+      this.setData({
+        displayCourse: array
+      });
+      console.log(this.data.displayCourse);
+    }
   },
 
   /**
