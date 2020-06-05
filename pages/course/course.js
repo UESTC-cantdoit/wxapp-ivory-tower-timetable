8323366c8dda56ad16c927879fd91ec971256d01
宋璟珅 <lolipop0703@qq.com>
2020-06-05 19:20:01 +0800

    save and load
    重新编写课程表页，留个备份

--- pages/course/course.js
+++ pages/course/course.js
@@ -5,23 +5,49 @@ Page({
    */
   data: {
     displayDayNum: 7, // 显示的天数，可选值为“5”和“7”
-    displayDay: ['一', '二', '三', '四', '五', '六', '七'],
+    displayDay: ['一', '二', '三', '四', '五', '六', '日'], // 根据 displayDayNum 生成
     displayCourseNum: 14, // 显示的每天课程数，可选值为“11”至“14”
-    displayCourse: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14],
+    displayCourse: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], // 根据 displayCourseNum 生成
     colorArrays: ["#FC9F9D", "#0A9A84"],
-    wlist: [
-      { "xqj": 1, "sksj": 1, "skcd":2, "kcxx": "微积分 品学楼101 1~16周"},
-      { "xqj": 1, "sksj": 8, "skcd": 2, "kcxx":"通用英语 品学楼318 1~16周"},
-      { "xqj": 2, "sksj": 3, "skcd": 2, "kcxx": "计算机组成原理 品学楼210 1~16周" },
-      { "xqj": 2, "sksj": 6, "skcd": 2, "kcxx": "C语言 品学楼402 1~16周" },
-      { "xqj": 3, "sksj": 3, "skcd": 2, "kcxx": "java 品学楼216 4~12周" },
-      { "xqj": 3, "sksj": 6, "skcd": 2, "kcxx": "数据结构与算法 品学楼306 1~16周" },
-      { "xqj": 4, "sksj": 1, "skcd": 2, "kcxx": "微积分 品学楼218 1~16周" },
-      { "xqj": 4, "sksj": 6, "skcd": 2, "kcxx": "乒乓球 体育馆 1~16周" },
-      { "xqj": 4, "sksj": 8, "skcd": 2, "kcxx": "数据结构与算法 品学楼216 4~16周" },
-      { "xqj": 4, "sksj": 10, "skcd": 2, "kcxx": "线性代数 品学楼504 1~16周" },
-      { "xqj": 5, "sksj": 3, "skcd": 2, "kcxx": "软件工程 品学楼403 1~16周" },
-    ]
+    course: [
+      {
+        "courseId": 1,
+        "courseName": '微积分',
+        "courseTeacher": '李xx',
+        "coursePlace": '品学楼 201',
+        "courseTime": [ // 上课时间
+          {
+            day: '3', // 周三
+            startTime: '3', // 第三节课开始到
+            endTime: '4'  // 第四节课结束
+          },
+          {
+            day: '4',
+            startTime: '7',
+            endTime: '8'
+          },
+        ]
+      },
+      {
+        "courseId": 2,
+        "courseName": '数据结构与设计',
+        "courseTeacher": '王xx',
+        "coursePlace": '立人楼 515',
+        "courseTime": [
+          {
+            day: '1',
+            startTime: '1',
+            endTime: '1'
+          },
+          {
+            day: '2',
+            startTime: '2',
+            endTime: '2'
+          },
+        ]
+      },
+    ],
+    showPopup: false
   },
 
   showCardView: function (e) {
@@ -30,15 +56,22 @@ Page({
     });
   },
 
+  showPopup() {
+    this.setData({
+      showPopup: true
+    });
+  },
+
+  hidePopup() {
+    this.setData({
+      showPopup: false
+    });
+  },
+
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    var kcxx = wx.getStorageSync('kcxx');
-    var skcd = wx.getStorageSync('skcd');
-    this.setData({ kcxx: kcxx });
-    this.setData({ skcd: skcd});
-
+  onLoad: function () {
     // 获取显示的天数，更新 displayDay
     if (this.data.displayDayNum != 7) {
       this.setData({
