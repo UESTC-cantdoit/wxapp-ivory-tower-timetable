c3446c994e044d759b792c4dc3b3f575c143798e
宋璟珅 <lolipop0703@qq.com>
2020-06-07 19:21:47 +0800

    update course timetable page
    更新
    - 为创建课程页添加上课地点栏
    - 重新设计课程表页
    
    待更新
    - 设计课程表页配色、字体等

--- pages/course/course.js
+++ pages/course/course.js
@@ -4,50 +4,50 @@ Page({
    * 页面的初始数据
    */
   data: {
-    displayDayNum: 7, // 显示的天数，可选值为“5”和“7”
+    displayDayNum: 5, // 由偏好设置获取；显示的天数，可选值为“5”和“7”
     displayDay: ['一', '二', '三', '四', '五', '六', '日'], // 根据 displayDayNum 生成
-    displayCourseNum: 14, // 显示的每天课程数，可选值为“11”至“14”
+    displayCourseNum: 14, // 由偏好设置获取；显示的每天课程数，可选值为“11”至“14”
     displayCourse: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], // 根据 displayCourseNum 生成
-    colorArrays: ["#FC9F9D", "#0A9A84"],
-    course: [
+    course: [ // 由数据库获取
       {
-        "courseId": 1,
-        "courseName": '微积分',
-        "courseTeacher": '李xx',
-        "coursePlace": '品学楼 201',
-        "courseTime": [ // 上课时间
+        courseId: 1,
+        courseName: '微积分',
+        courseTeacher: '李xx',
+        coursePlace: '品学楼 201',
+        courseTime: [ // 上课时间
           {
-            day: '3', // 周三
-            startTime: '3', // 第三节课开始到
-            endTime: '4'  // 第四节课结束
+            day: 3, // 周三
+            startTime: 3, // 第三节课开始到
+            endTime: 4  // 第四节课结束
           },
           {
-            day: '4',
-            startTime: '7',
-            endTime: '8'
+            day: 4,
+            startTime: 7,
+            endTime: 8
           },
         ]
       },
       {
-        "courseId": 2,
-        "courseName": '数据结构与设计',
-        "courseTeacher": '王xx',
-        "coursePlace": '立人楼 515',
-        "courseTime": [
+        courseId: 2,
+        courseName: '数据结构与设计',
+        courseTeacher: '王xx',
+        coursePlace: '立人楼 515',
+        courseTime: [
           {
-            day: '1',
-            startTime: '1',
-            endTime: '1'
+            day: 1,
+            startTime: 1,
+            endTime: 1
           },
           {
-            day: '2',
-            startTime: '2',
-            endTime: '2'
+            day: 2,
+            startTime: 2,
+            endTime: 2
           },
         ]
       },
     ],
-    showPopup: false
+    showPopup: false,
+    courseTimetable: [], // onLoad 中加载生成，处理 course 信息并添加到此处
   },
 
   showCardView: function (e) {
@@ -71,16 +71,19 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function () {
+  onLoad: function (options) {
+    const displayDayNum = this.data.displayDayNum;
+    const displayCourseNum = this.data.displayCourseNum;
+
     // 获取显示的天数，更新 displayDay
-    if (this.data.displayDayNum != 7) {
+    if (displayDayNum != 7) {
       this.setData({
         displayDay: ['一', '二', '三', '四', '五']
       });
     }
 
     // 获取显示的课程节数，更新 displayCourse
-    if (this.data.displayCourseNum != 14) {
+    if (displayCourseNum != 14) {
       const num = this.data.displayCourseNum;
       let array = [];
       for (let count = 1; count <= num; count++) {
@@ -89,8 +92,47 @@ Page({
       this.setData({
         displayCourse: array
       });
-      console.log(this.data.displayCourse);
     }
+
+    // 处理 course 信息添加到 courseTimetable 进行渲染
+    let courseTimetable = [];
+    for (let i = 0; i < ((displayDayNum + 1) * displayCourseNum); i++) {
+      if ((i % (displayDayNum + 1)) == 0) { // 左侧课程节数
+        courseTimetable.push({
+          gridId: i,
+          courseNum: i/(displayDayNum+1) + 1
+        });
+      } else {
+        courseTimetable.push({  // 课程初始化
+          gridId: i,
+          courseId: null,
+          courseName: '',
+          coursePlace: ''
+        });
+      }
+    };
+    const course = this.data.course;
+    for (let i = 0; i < course.length; i++) { // 依次获取课程信息
+      const courseId = course[i].courseId;
+      const courseName = course[i].courseName;
+      const coursePlace = course[i].coursePlace;
+      const courseTime = course[i].courseTime;
+      for (let n = 0; n < courseTime.length; n++) { // 依次获取该课程的上课时间信息
+        const day = courseTime[n].day;
+        const startTime = courseTime[n].startTime;
+        const endTime = courseTime[n].endTime;
+        for (let t = startTime; t <= endTime; t++) {
+          courseTimetable[(t - 1) * (displayDayNum + 1) + day] = ({
+            courseId: courseId,
+            courseName: courseName,
+            coursePlace: coursePlace
+          })
+        };
+      };
+    };
+    this.setData({
+      courseTimetable: courseTimetable
+    });
   },
 
   /**
@@ -103,7 +145,7 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function () { // 若修改过每周显示天数或每天显示课程数，则重新加载本页面
    
   },
 
